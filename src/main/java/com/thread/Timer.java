2d322789923da25c5063c81044fb0e84716d660d
zahitkaya <zahit_kaya58@hotmail.com>
2020-10-14 09:29:26 +0300

    Lambda

--- src/main/java/com/thread/Timer.java
+++ src/main/java/com/thread/Timer.java
@@ -4,30 +4,29 @@ public class Timer extends Thread {
     private int remainingTime;
     private String alarmName;
 
-    public Timer(int remainingTime,String alarmName){
-        this.alarmName=alarmName;
-        this.remainingTime=remainingTime;
+    public Timer(int remainingTime, String alarmName) {
+        this.alarmName = alarmName;
+        this.remainingTime = remainingTime;
     }
 
     public static void main(String[] args) {
-        Timer alarm=new Timer(60,"İlk alarm");
-        Timer alarm2=new Timer(80,"İkinci alarm");
+        Timer alarm = new Timer(60, "İlk alarm");
+        Timer alarm2 = new Timer(80, "İkinci alarm");
         alarm.start();
         alarm2.start();
     }
 
     @Override
     public void run() {
-        try{
-            while(remainingTime>=0){
+        try {
+            while (remainingTime >= 0) {
                 System.out.println();
-                System.out.println(alarmName+" Zamanın bitmesine "+ remainingTime +" saniye kaldı");
+                System.out.println(alarmName + " Zamanın bitmesine " + remainingTime + " saniye kaldı");
                 System.out.println();
                 Thread.sleep(1000);
                 remainingTime--;
             }
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
